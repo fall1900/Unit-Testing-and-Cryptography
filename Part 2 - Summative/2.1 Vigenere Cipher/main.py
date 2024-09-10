c93ac54492d26a33a97c3df0febe8b099e1e1b9a
200689 <2026venkateshwaran.vidyut@fcboe.org>
2024-09-10 11:11:01 -0400

    finished most of encode part of vig cipher

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -2,20 +2,33 @@
 
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 def vig_encode(text, keyword):
-  new_str = ""
-  for let in text:
-    index_a = alpha.index(let)
-    index_b = alpha.index(keyword[index_a])
-    index_sum = (index_a + index_b) % 26
-    new_str += alpha[index_sum]
-  return new_str
+    new_str = ""
+    extra_word = ""
+    if len(text) % len(keyword) == 0:
+      times_printed = int(len(text) / len(keyword))
+      print(times_printed)
+    else:
+      for i in range(len(text) % len(keyword)):
+        extra_word += keyword[i]
+      times_printed = int((len(text) - len(extra_word)) / len(keyword))
+      print(times_printed)
+    keyword_str = keyword * times_printed
+    print(keyword_str + extra_word)
+    for let in text:
+        index_a = alpha.index(let)
+        for word in keyword_str:
+          index_b = alpha.index(word)
+        index_sum = (index_a + index_b)
+        new_str += alpha[index_sum - 26]
+    return new_str
 
 
 def vig_decode(text, keyword):
-  new_str = ""
+    new_str = ""
 
-  return new_str
+    return new_str
 
 
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
