382a2a6a1c68a4500ecff9092d7c80d8c72ddb7e
200689 <2026venkateshwaran.vidyut@fcboe.org>
2024-09-11 11:10:42 -0400

    finished most of encode part of vig cipher, have to fix output

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -4,24 +4,27 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def vig_encode(text, keyword):
+    text = text.upper()
+    keyword = keyword.upper()
     new_str = ""
     extra_word = ""
     if len(text) % len(keyword) == 0:
       times_printed = int(len(text) / len(keyword))
-      print(times_printed)
     else:
       for i in range(len(text) % len(keyword)):
         extra_word += keyword[i]
       times_printed = int((len(text) - len(extra_word)) / len(keyword))
-      print(times_printed)
     keyword_str = keyword * times_printed
-    print(keyword_str + extra_word)
+    full_keyword = keyword_str + extra_word
     for let in text:
-        index_a = alpha.index(let)
-        for word in keyword_str:
-          index_b = alpha.index(word)
-        index_sum = (index_a + index_b)
-        new_str += alpha[index_sum - 26]
+        if let not in alpha:
+            new_str += let
+        else:
+            index_a = alpha.index(let)
+            for let in keyword_str:
+                index_b = alpha.index(let)
+            index_sum = (index_a + index_b)
+            new_str += alpha[index_sum - 26]
     return new_str
 
 
@@ -38,3 +41,32 @@ dec = vig_decode(enc, vig_key)
 print(enc)
 print(dec)
 # If this worked, dec should be the same as test!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
