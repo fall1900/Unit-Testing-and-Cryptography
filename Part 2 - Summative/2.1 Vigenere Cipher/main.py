8d58cf96c146665322f773f950a40214b00fc31f
200689 <2026venkateshwaran.vidyut@fcboe.org>
2024-09-12 11:02:49 -0400

    finished vig_encode and vig_decode, started working on unit testing

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -16,21 +16,41 @@ def vig_encode(text, keyword):
       times_printed = int((len(text) - len(extra_word)) / len(keyword))
     keyword_str = keyword * times_printed
     full_keyword = keyword_str + extra_word
-    for let in text:
-        if let not in alpha:
-            new_str += let
-        else:
-            index_a = alpha.index(let)
-            for let in keyword_str:
-                index_b = alpha.index(let)
-            index_sum = (index_a + index_b)
-            new_str += alpha[index_sum - 26]
+    for i in range(len(text)):
+        text_let = text[i]
+        keyword_let = full_keyword[i]
+        index_a = alpha.index(text_let)
+        index_b = alpha.index(keyword_let)
+
+        index_sum = (index_a + index_b)
+        new_str += alpha[index_sum - 26]
     return new_str
 
 
 def vig_decode(text, keyword):
+    text = text.upper()
+    keyword = keyword.upper()
     new_str = ""
-
+    extra_word = ""
+    if len(text) % len(keyword) == 0:
+        times_printed = int(len(text) / len(keyword))
+    else:
+        for i in range(len(text) % len(keyword)):
+            extra_word += keyword[i]
+        times_printed = int((len(text) - len(extra_word)) / len(keyword))
+    keyword_str = keyword * times_printed
+    full_keyword = keyword_str + extra_word
+    for i in range(len(text)):
+        text_let = text[i]
+        keyword_let = full_keyword[i]
+        index_a = alpha.index(text_let)
+        index_b = alpha.index(keyword_let)
+        index_sum = (index_a - index_b)
+        if index_sum < 0:
+            index_sum += 26
+        else:
+            index_sum -= 26
+        new_str += alpha[index_sum]
     return new_str
 
 
